b5a6d011004c0fdb1965a466ae80bb445b3a735b
Swooshoo <eliotlee1415@gmail.com>
2024-02-17 19:50:58 -0800

    added csv, and @app.route/creditcards

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,51 @@ import React, {useState, useEffect} from 'react'
 function App() {
 	const [data, setData] = useState([{}])
 
-	useEffect(() => {
-		fetch("/members").then(
-			res => res.json()
-		).then(
-			data => {
-				setData(data)
-				console.log(data)
-			}
-		)
-	}, [])
+// 	useEffect(() => {
+// 		fetch("/members").then(
+// 			res => res.json()
+// 		).then(
+// 			data => {
+// 				setData(data)
+// 				console.log(data)
+// 			}
+// 		)
+// 	}, [])
+
+//   return (
+//     <div>
+// 		{(typeof data.members === 'undefined') ? (
+// 			<p>Loading...</p>
+// 		) : (
+// 			data.members.map((member, i) => (
+// 				<p key={i}>{member}</p>
+// 			))
+// 		)}
+//     </div>
+//   )
+
+  useEffect(() => {
+	fetch("/creditcards").then(
+		res => res.json()
+	).then(
+		data => {
+			setData(data)
+			console.log(data)
+		}
+	)
+}, [])
+
+return (
+<div>
+	{(typeof data.creditcards === 'undefined') ? (
+		<p>Loading...</p>
+	) : (
+		data.creditcards.map((creditcard, i) => (
+			<p key={i}>{creditcard}</p>
+		))
+	)}
+</div>
+)
 
   return (
     <div>
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,28 @@
+import csv
 from flask import Flask
 
 app = Flask(__name__)
 
+CSV_FILE = "creditcards.csv"
+
+def read_credit_cards_from_csv(csv_file):
+    credit_cards = []
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row if it exists
+        for row in reader:
+            credit_card = row[0] + " " + row[1]  # Combine column 1 and column 2
+            credit_cards.append(credit_card)
+    return credit_cards
+
 @app.route("/members")
 def members():
     return {"members" : ["Member1", "Member2", "Member3"]}
+
+@app.route("/creditcards")
+def creditcards():
+    credit_cards = read_credit_cards_from_csv(CSV_FILE)
+    return {"creditcards": credit_cards}
 
 if __name__ == "__main__":
     app.run(debug=True)
